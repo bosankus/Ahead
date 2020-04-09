2aaf4512da6cf08ba2ddf0369fe707a1c44493bc
Ankush Bose <ankushbose5@gmail.com>
2020-04-10 03:38:38 +0530

    Initial Commit

---
+++ app/src/main/java/tech/androidplay/sonali/todo/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package tech.androidplay.sonali.todo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tech.androidplay.sonali.todo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
