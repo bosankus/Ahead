92432fe2ca4cb83f3ff98bd7d966aec650dc361d
Ankush Bose <ankushbose5@gmail.com>
2020-04-27 05:28:33 +0530

    Logout implemented. Few assets changed.

--- app/src/main/java/tech/androidplay/sonali/todo/view/ProfileFragment.kt
+++ app/src/main/java/tech/androidplay/sonali/todo/view/ProfileFragment.kt
@@ -1,17 +1,23 @@
 package tech.androidplay.sonali.todo.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_profile.*
 import tech.androidplay.sonali.todo.R
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        firebaseAuth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
@@ -24,6 +30,17 @@ class ProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        btnSignOut.setOnClickListener {
+            firebaseAuth.signOut()
+            goToLoginActivity()
+        }
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(activity, SplashActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 
     companion object {
