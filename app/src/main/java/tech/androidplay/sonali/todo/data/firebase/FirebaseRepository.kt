f5cc4fe65a2545b0103d2823bbd832d8984d479c
Ankush Bose <ankushbose5@gmail.com>
2020-12-24 08:51:01 +0530

    ADDED: User.kt added * This will help to perform collaboration of users for task * Collaboration fields are not yet added * Profile details will be created later or in this update.

--- app/src/main/java/tech/androidplay/sonali/todo/data/firebase/FirebaseRepository.kt
+++ app/src/main/java/tech/androidplay/sonali/todo/data/firebase/FirebaseRepository.kt
@@ -5,6 +5,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.storage.StorageReference
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
@@ -58,12 +59,14 @@ class FirebaseRepository @Inject constructor(
             val response = firebaseAuth
                 .createUserWithEmailAndPassword(email, password)
                 .await()
-            val userMap = mapOf(
+            val userMap = hashMapOf(
                 "uid" to response.user?.uid,
                 "email" to response.user?.email,
                 "createdOn" to getCurrentTimestamp()
             )
-            userListRef.add(userMap).await()
+            response?.user?.let {
+                userListRef.document("${it.email}").set(userMap, SetOptions.merge()).await()
+            } ?: return ResultData.Failed("Failed to create user")
             ResultData.Success(response.user)
         } catch (e: Exception) {
             ResultData.Failed(e.message)
